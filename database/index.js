5d81764cf0517a906bfef443ff5abc0b172a51da
Drew Nguyen <drew.ktran@gmail.com>
2019-07-30 15:59:46 -0700

    changed database index.js

--- database/index.js
+++ database/index.js
@@ -2,29 +2,39 @@ const { Pool } = require("pg");
 //change the url to use env varibales
 let url = "postgres://qvgmlczt:XBaX9s...@raja.db.elephantsql.com:5432/qvgmlczt";
 const pool = new Pool({
-  coconnectionString: u,
-})});
+  connectionString: url
+});
 
 // creating table of visitors
 pool.query(
-  if(err) throw err;
-console.log()visitrs table created", result);
- 
-  });
+  "CREATE TABLE IF NOT EXISTS visitors (_id SERIAL PRIMARY KEY, fisrtname VARCHAR, lastname VARCHAR, reason VARCHAR, date TIMESTAMP)",
+  (err, result) => {
+    if (err) throw err;
+    console.log("visitors table created", result);
+  }
+);
 
 //creating table of admin/pin
-poerr, result) => {
-   if (err) throw err;
-//     console.log( admin table reated", result);
-  });
+pool.query(
+  "CREATE TABLE IF NOT EXISTS admin (_id SERIAL PRIMARY KEY, pin INTEGER, username VARCHAR)",
+  (err, result) => {
+    if (err) throw err;
+    console.log("admin table created", result);
+  }
+);
 
-emailusername, email VARCHAR//creating table for slack
-pool.query()CREATE TABLerr, result) => {
-    if(err) throw err;
-   onsole.log("signin table created", result);
-  );
+//creating table for slack
+pool.query(
+  "CREATE TABLE IF NOT EXISTS signin (_id SERIAL PRIMARY KEY, visitor_id INTEGER, admin_id INTEGER, date TIMESTAMP)",
+  (err, result) => {
+    if (err) throw err;
+    console.log("signin table created", result);
+  }
+);
 
-////exporting query function for server requests
-module.expoery: (text, params, callback) => {
-  " return pool.query(text, params, callback)IAL PRIMARY KEY, visitor_id INTEGER, admin_id INTEGER, date TIMESTAMP)",
-}};// // // // // // // // 
+//exporting query function for server requests
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  }
+};
