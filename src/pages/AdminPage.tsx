985d7e49cdab093771384776e66f0b992771634f
Drew Nguyen <drew.ktran@gmail.com>
2019-08-01 10:56:40 -0700

    added some table styling

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,46 +3,41 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button";
 
 function AdminPage(props: any) {
-  let rowsData:any = [];
-  for(let i = 0; i < props.tableData.length; i++){
+  let rowsData: any = [];
+  for (let i = 0; i < props.tableData.length; i++) {
     rowsData.push(
-    <tr>
-      <th>
-        {props.tableData[i].firstname}
-      </th>
-      <th>
-        {props.tableData[i].lastname}
-      </th>
-      <th>
-        {props.tableData[i].username}
-      </th>
-      <th>
-        {props.tableData[i].reason}
-      </th>
-      <th>
-        {props.tableData[i].date}
-      </th>
-    </tr>)
+      <tr className="content">
+        <td>{props.tableData[i].firstname}</td>
+        <td>{props.tableData[i].lastname}</td>
+        <td>{props.tableData[i].username}</td>
+        <td>{props.tableData[i].reason}</td>
+        <td>{props.tableData[i].date}</td>
+      </tr>
+    );
   }
   return (
-  <div>
-    <Link to="/">
-      <Button buttonName="Back" />
-    </Link>
-    Check-in Log
-    <table>
-      <tbody>
-        <tr>
-          <th>Visitor's Firstname</th>
-          <th>Visitor's Lastname</th>
-          <th>CodeSmith Collaborator</th>
-          <th>Reason for visit</th>
-          <th>Date</th>
-        </tr>
-        {rowsData}
-      </tbody>
-    </table>
-  </div>
+    <div>
+      <div className="back-button">
+        <Link to="/">
+          <input className="submit-back" type="submit" value="< Back" />
+        </Link>
+      </div>
+      <p className="h-admin">Check-in Log</p>
+      <div className="table">
+        <table>
+          <tbody>
+            <tr className="tr-header">
+              <th>Visitor's Firstname</th>
+              <th>Visitor's Lastname</th>
+              <th>CodeSmith Collaborator</th>
+              <th>Reason for visit</th>
+              <th>Date</th>
+            </tr>
+            {rowsData}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
