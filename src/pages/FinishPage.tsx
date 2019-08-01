70cc120cc216e4e636d2fec715cb9944afdd3a9a
Jushuworld <jushuworld@gmail.com>
2019-07-31 21:51:30 -0700

    done finish page

--- src/pages/FinishPage.tsx
+++ src/pages/FinishPage.tsx
@@ -3,13 +3,18 @@ import styled from "styled-components";
 
 const FinishPage: React.FunctionComponent<{}> = (props: any) => {
   // need to implement a more react-type solution
-  // window.setTimeout(() => {
-  //   window.location.href = "http://localhost:3000";
-  // }, 5000);
+  console.log("✅", props.location.type);
+  window.setTimeout(() => {
+    window.location.href = "http://localhost:3000";
+  }, 5000);
   return (
     <FinishStyled>
-      <img src='/src/images/codesmith.svg' style={{height:'30vh', width:'30vw'}} alt='Logo' />
-      <FirstStyled>Welcome, </FirstStyled>
+      <img
+        src='/src/images/codesmith.svg'
+        style={{ height: "30vh", width: "30vw" }}
+        alt='Logo'
+      />
+      <FirstStyled>Welcome, {props.location.state.type} </FirstStyled>
       <SecondStyled>Have a seat, Someone will be with you shortly</SecondStyled>
     </FinishStyled>
   );
@@ -20,24 +25,24 @@ const FinishStyled = styled.div`
   flex-direction: column;
   align-items: center;
   border: 2px solid black;
-  height: 100vh
+  height: 100vh;
+  width: 100%;
   justify-content: center;
-  
 `;
 
 const FirstStyled = styled.div`
-  font-size: 10rem;
+  font-size: 5rem;
   font-weight: bold;
   color: rgb(54, 74, 143);
-  border: 2px solid green;
+  // border: 2px solid green;
   margin: 1rem;
 `;
 
 const SecondStyled = styled.div`
-  font-size: 10em;
+  font-size: 5rem;
   width: 60vw;
   color: rgb(54, 74, 143);
-  border: 2px solid blue;
+  // border: 2px solid blue;
   margin: 1rem;
 `;
 
