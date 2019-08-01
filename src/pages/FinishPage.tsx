9c21c0d43e846ab267d07e156f19a5d2e8878a84
Jushuworld <jushuworld@gmail.com>
2019-07-31 20:03:37 -0700

    before merge

--- src/pages/FinishPage.tsx
+++ src/pages/FinishPage.tsx
@@ -5,7 +5,7 @@ function FinishPage(props: any) {
   // need to implement a more react-type solution
   window.setTimeout(() => {
     window.location.href = "http://localhost:3000";
-  }, 3000);
+  }, 5000);
   return <div>Done! Finish page</div>;
 }
 
