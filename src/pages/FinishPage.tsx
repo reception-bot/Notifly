a6115c99f13b0188854a0e3e90322d472c078edb
Jushuworld <jushuworld@gmail.com>
2019-07-31 20:49:52 -0700

    done finish page

--- src/pages/FinishPage.tsx
+++ src/pages/FinishPage.tsx
@@ -1,12 +1,44 @@
 import * as React from "react";
-import { Route, Redirect } from "react-router";
+import styled from "styled-components";
 
-function FinishPage(props: any) {
+const FinishPage: React.FunctionComponent<{}> = (props: any) => {
   // need to implement a more react-type solution
-  window.setTimeout(() => {
-    window.location.href = "http://localhost:3000";
-  }, 5000);
-  return <div>Done! Finish page</div>;
-}
+  // window.setTimeout(() => {
+  //   window.location.href = "http://localhost:3000";
+  // }, 5000);
+  return (
+    <FinishStyled>
+      <img src='/src/images/codesmith.svg' style={{height:'30vh', width:'30vw'}} alt='Logo' />
+      <FirstStyled>Welcome, </FirstStyled>
+      <SecondStyled>Have a seat, Someone will be with you shortly</SecondStyled>
+    </FinishStyled>
+  );
+};
+
+const FinishStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2px solid black;
+  height: 100vh
+  justify-content: center;
+  
+`;
+
+const FirstStyled = styled.div`
+  font-size: 10rem;
+  font-weight: bold;
+  color: rgb(54, 74, 143);
+  border: 2px solid green;
+  margin: 1rem;
+`;
+
+const SecondStyled = styled.div`
+  font-size: 10em;
+  width: 60vw;
+  color: rgb(54, 74, 143);
+  border: 2px solid blue;
+  margin: 1rem;
+`;
 
 export default FinishPage;
